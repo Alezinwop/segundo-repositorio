3c7799bd01f54e4f98a23ea2303828dcdb06c464
Alezinwop <138633873+Alezinwop@users.noreply.github.com>
2024-10-22 22:02:01 -0300

    22/10/24

---
+++ mvv.js
@@ -0,0 +1,17 @@
+criaCartao (
+    'Missão',
+    'Saiba qual é a Missão de nossa',
+    'Nossa Missão é ...',
+)
+
+criaCartao (
+    'Visão',
+    'Saiba qual é a Visão de nossa Empresa',
+    'Nossa Visão é ... ',
+)
+
+criaCartao (
+    'Valores',
+    'Saiba qual é a Valores de nossa Empresa',
+    'Nossa Valores são ...',
+)
