77f20d09a66330fec2169a1984fed6aee27ac008
Alezinwop <138633873+Alezinwop@users.noreply.github.com>
2024-11-05 21:40:01 -0300

    Aula 5 do 11

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 function criaCartao (item, pergunta,resposta) {
-    console.log(item,pergunta, resposta) 
+    //console.log(item,pergunta, resposta)
+    
+    let flashcard = document.getElementById('flashcard');
+    let cartao = document.createElement('article');
+
+    cartao.className = 'cartao';
+
+    cartao.innerHTML = 
+   <div class="cartao__conteudo">
+        <h3>Missão</h3>
+    </div>
+    <div class="cartao__conteudo__pergunta">
+      <p>Saiba qual é a Missão de noesaaaaa</p>  
+    </div>
+
+    <div class="cartao__conteudo__resposta"> 
+        <p> Nossa Missão é ...</p>
+     </div>
+    </div>
+
+    flashcard.appendChield(cartao);
+ 
+
+
+
+
 }
