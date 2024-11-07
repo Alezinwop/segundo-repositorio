7c328c05f251c1f415297820254c6a8c80d198fe
Alezinwop <138633873+Alezinwop@users.noreply.github.com>
2024-11-06 21:56:46 -0300

    Aula 9 06/11

--- app.js
+++ app.js
@@ -6,19 +6,29 @@ function criaCartao (item, pergunta,resposta) {
 
     cartao.className = 'cartao';
 
-    cartao.innerHTML = 
-    <div class="cartao__conteudo">
-    <h3>${item}</h3>
+    cartao.innerHTML = `
+     <div class="cartao__conteudo">
+                <h3>${item}</h3>
 
-    <div class="cartao__conteudo__pergunta">
+            <div class="cartao__conteudo__pergunta">
 
-    <p>${pergunta}</p>  
-    </div>
+                <p>${pergunta}</p>  
+                </div>
 
-    <div class="cartao__conteudo__resposta"> 
-    <p>${resposta}</p>
+            <div class="cartao__conteudo__resposta"> 
+                <p>${resposta}</p>
      </div>
     </div>
+`
+
+    let respostaEstaVisivel = false 
+
+    function viraCartao () {
+        respostaEstaVisivel = !variavelEstaVisivel;
+        cartao.classList.toggle('active', viraCartao);
+    }
+
+    cartao.addEventListener('click', viraCartao)
 
     flashcard.appendChield(cartao);
  
